ff5d22d20866e30e4c0b007f03e5d3a89d25c849
Razan Masood <razan.masood@stud.uni-due.de>
2017-05-07 23:49:56 +0200

    Set the project

---
+++ TestWeka/src/main/java/test/weka/TestWeka/App.java
@@ -0,0 +1,13 @@
+package test.weka.TestWeka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
